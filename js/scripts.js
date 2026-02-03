4cd22f40ad842b5928267ce46b7a337157103b2d
Ran <xyfwdy@gmail.com>
2026-02-03 12:04:06 +0800

    修复 报错

--- js/scripts.js
+++ js/scripts.js
@@ -161,20 +161,6 @@
 		}
     });
 
-
-    /* Call Me Form */
-    $("#callMeForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            lformError();
-            lsubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            lsubmitForm();
-        }
-    });
-
     function lsubmitForm() {
         // initiate variables with form content
 		var name = $("#lname").val();
@@ -219,20 +205,6 @@
         $("#lmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
 
-
-    /* Contact Form */
-    $("#contactForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            cformError();
-            csubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            csubmitForm();
-        }
-    });
-
     function csubmitForm() {
         // initiate variables with form content
 		var name = $("#cname").val();
@@ -275,20 +247,6 @@
         }
         $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-
-
-    /* Privacy Form */
-    $("#privacyForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            pformError();
-            psubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            psubmitForm();
-        }
-    });
 
     function psubmitForm() {
         // initiate variables with form content
