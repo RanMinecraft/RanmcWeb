31334d61afe6112878e6b82dc8afe6129f8df00b
Ran <xyfwdy@gmail.com>
2026-02-04 17:37:25 +0800

    修复 回到顶部按钮

--- constant/groupChat.js
+++ constant/groupChat.js
@@ -1,3 +1,3 @@
 document.writeln(`
-<a class="btn-solid-reg page-scroll" href="https://qm.qq.com/q/vKrsJNZsNG" target="_blank">点击加群</a></h3>
+<a class="btn-solid-reg" href="https://qm.qq.com/q/vKrsJNZsNG" target="_blank">点击加群</a></h3>
 `)
--- constant/header.js
+++ constant/header.js
@@ -9,26 +9,26 @@ document.writeln(`    <nav class="navbar navbar-expand-md navbar-dark navbar-cus
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="index.html#header">首页</span></a>
+                    <a class="nav-link" href="index.html#header">首页</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="index.html#info">服务器介绍</a>
+                    <a class="nav-link" href="index.html#info">服务器介绍</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle page-scroll" href="./index.html#list" id="navbarDropdown"
+                    <a class="nav-link dropdown-toggle" href="./index.html#list" id="navbarDropdown"
                         role="button" aria-haspopup="true" aria-expanded="false">服务器列表</a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="server.html"><span class="item-text">纯净生存 <script type="text/javascript" src="constant/version.js"></script> </span></a>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="index.html#picture">风景欣赏</a>
+                    <a class="nav-link" href="index.html#picture">风景欣赏</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="news.html#header">游戏公告</a>
+                    <a class="nav-link" href="news.html#header">游戏公告</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle page-scroll" href="banlist.html#header" id="navbarDropdown"
+                    <a class="nav-link dropdown-toggle" href="banlist.html#header" id="navbarDropdown"
                         role="button" aria-haspopup="true" aria-expanded="false">封禁列表</a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="service.html?type=bug#header"><span class="item-text">反馈建议</span></a>
@@ -41,7 +41,7 @@ document.writeln(`    <nav class="navbar navbar-expand-md navbar-dark navbar-cus
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="card.html" target="_blank">新年贺卡</a>
+                    <a class="nav-link" href="card.html" target="_blank">新年贺卡</a>
                 </li>
             </ul>
         </div>
