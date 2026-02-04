2bfa95dbaa12773f54ee879d405c21033b483149
Ran <xyfwdy@gmail.com>
2026-02-04 23:10:35 +0800

    修复 手机端图片堆叠

--- js/index.js
+++ js/index.js
@@ -29,5 +29,10 @@
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue });
     });
+
+    // 修复手机端卡牌都堆叠在一起问题 分辨率大小变化的时候刷新ui
+    $(window).on('resize', function() {
+      $grid.isotope('layout');
+    });
     
 })(jQuery);
